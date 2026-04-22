d937eb34511d31e973edfee58f5669a6d0b0c2fb
aminaselmi <minaselmi12@gmail.com>
2026-04-22 16:46:23 +0100

    fix: include build folder for render

--- backend/server.js
+++ backend/server.js
@@ -38,14 +38,14 @@ app.use(cors());
 
 
 
-const path = require("path");
+//const path = require("path");
 
 // Serve React build (go one level up)
-app.use(express.static(path.join(__dirname, "../build")));
+//app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
+//app.get("*", (req, res) => {
+ // res.sendFile(path.join(__dirname, "../build", "index.html"));
+//});
 
 const PORT = process.env.PORT || 5000;
 
