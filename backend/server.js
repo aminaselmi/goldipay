02205581fb52e659b3a2d976dd34369e8e755477
aminaselmi <minaselmi12@gmail.com>
2026-04-22 18:35:33 +0100

    fix: react routing on render

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const path = require("path");
 //Serve React build (go one level up)
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
